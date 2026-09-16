74e9bcdca4cde95a537a1acf0b233dca18fcf9e7
Cane <canicboban@gmail.com>
2026-09-16 09:32:45 +0200

    feat: version OLED PWA service worker

--- app/service-worker-register.tsx
+++ app/service-worker-register.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from "react";
 
-const SERVICE_WORKER_URL = "/sw.js?v=0.31-core-rdbi-1";
+const SERVICE_WORKER_URL = "/sw.js?v=0.32-oled-landing-1";
 
 export default function ServiceWorkerRegister() {
   useEffect(() => {
