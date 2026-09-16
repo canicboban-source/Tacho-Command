4b261a5530158f53d48aadcb1e90226bd96c46bc
Cane <canicboban@gmail.com>
2026-09-16 09:32:10 +0200

    feat: surface install guide on landing

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import InstallGuide from "./install-guide";
 import LandingPage from "./landing-page";
 
 export default function Home() {
-  return <LandingPage />;
+  return (
+    <>
+      <LandingPage />
+      <InstallGuide />
+    </>
+  );
 }
