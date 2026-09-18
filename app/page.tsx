2730f21a656762517823b76225b6b03dee30bbbf
Cane <canicboban@gmail.com>
2026-09-18 19:38:56 +0200

    feat: route admin hostname to private cockpit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { isAdminHost } from "../lib/admin-host.js";
 import InstallGuide from "./install-guide";
 import LandingPage from "./landing-page";
 
-export default function Home() {
+export default async function Home() {
+  const host = (await headers()).get("host");
+  if (isAdminHost(host)) redirect("/admin");
+
   return (
     <>
       <LandingPage />
